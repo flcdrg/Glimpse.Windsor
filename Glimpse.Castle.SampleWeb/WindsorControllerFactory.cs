5d2cca97fbfb7c631234301e1409a6e1a05be99f
David Gardiner <david@gardiner.net.au>
2015-06-19 23:50:20 +0930

    glimpse working in sample app

--- Glimpse.Castle.SampleWeb/WindsorControllerFactory.cs
+++ Glimpse.Castle.SampleWeb/WindsorControllerFactory.cs
@@ -1,22 +1,17 @@
 ﻿using System;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 using Castle.MicroKernel;
-using Castle.MicroKernel.Registration;
-using Castle.MicroKernel.SubSystems.Configuration;
-using Castle.Windsor;
 
 namespace Glimpse.Castle.SampleWeb
 {
-
     public class WindsorControllerFactory : DefaultControllerFactory
     {
         private readonly IKernel _kernel;
 
         public WindsorControllerFactory(IKernel kernel)
         {
-            this._kernel = kernel;
+            _kernel = kernel;
         }
 
         public override void ReleaseController(IController controller)
@@ -33,16 +28,6 @@ namespace Glimpse.Castle.SampleWeb
             }
 
             return base.GetControllerInstance(requestContext, controllerType);
-        }
-    }
-
-    public class ControllersInstaller : IWindsorInstaller
-    {
-        public void Install(IWindsorContainer container, IConfigurationStore store)
-        {
-            container.Register(Classes.FromThisAssembly()
-                .BasedOn<IController>()
-                .LifestyleTransient());
         }
     }
 }
