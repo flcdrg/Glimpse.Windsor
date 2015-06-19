73d36d9978d6ce796efdbe85c5dfcce270b1d48f
David Gardiner <david@gardiner.net.au>
2015-06-19 22:50:04 +0930

    Use local copy of KernalDebuggerProxy

---
+++ Glimpse.Castle/CastleGlimpsePlugin.cs
@@ -0,0 +1,36 @@
+﻿extern alias CastleWindsor;
+using System.Collections.Generic;
+using System.Linq;
+using CastleWindsor::Castle.Windsor;
+using Glimpse.Core.Extensibility;
+
+namespace Glimpse.Castle
+{
+    extern alias CastleWindsor;
+
+    public class CastleGlimpsePlugin : TabBase
+    {
+        internal static IWindsorContainer Container;
+
+        public override object GetData(ITabContext context)
+        {
+            if (Container == null)
+                return null;
+
+            var proxy = new KernelDebuggerProxy(Container);
+
+            var data = new List<object[]> { new object[] { proxy.Extensions.Select(ex => ex.Name) } };
+            data.AddRange(proxy.Extensions.Select(item => new[] {item.Value}));
+
+            return data;
+        }
+
+        public override string Name
+        {
+            get
+            {
+                return "Castle";
+            }
+        }
+    }
+}
---
+++ Glimpse.Castle/KernelDebuggerProxy.cs
@@ -0,0 +1,43 @@
+﻿extern alias CastleWindsor;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using CastleWindsor::Castle.MicroKernel;
+using CastleWindsor::Castle.Windsor;
+using CastleWindsor::Castle.Windsor.Diagnostics;
+using CastleWindsor::Castle.Windsor.Diagnostics.DebuggerViews;
+
+namespace Glimpse.Castle
+{
+    internal class KernelDebuggerProxy
+    {
+        private readonly IEnumerable<IContainerDebuggerExtension> _extensions;
+
+        public KernelDebuggerProxy(IWindsorContainer container)
+            : this(container.Kernel)
+        {
+        }
+
+        public KernelDebuggerProxy(IKernel kernel)
+        {
+            if (kernel == null)
+            {
+                throw new ArgumentNullException("kernel");
+            }
+            _extensions =
+                (IEnumerable<IContainerDebuggerExtension>)(kernel.GetSubSystem(SubSystemConstants.DiagnosticsKey) as IContainerDebuggerExtensionHost) ??
+                new IContainerDebuggerExtension[0];
+        }
+
+        [DebuggerDisplay("")]
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public DebuggerViewItem[] Extensions
+        {
+            get
+            {
+                return _extensions.SelectMany(e => e.Attach()).ToArray();
+            }
+        }
+    }
+}
