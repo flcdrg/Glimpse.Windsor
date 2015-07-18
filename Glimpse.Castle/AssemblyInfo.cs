20387f612dfad7bf16b52f82c4d91b6c0299bfa8
David Gardiner <david@gardiner.net.au>
2015-07-18 22:06:10 +0930

    Warnings as errors .NET4, .NET 4.5 and .NET 4.6 Move files to shared project

--- Glimpse.Castle/AssemblyInfo.cs
+++ Glimpse.Castle/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Glimpse.Castle")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Castle Windsor extension for Glimpse")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("David Gardiner")]
 [assembly: AssemblyProduct("Glimpse.Castle")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © David Gardiner 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
